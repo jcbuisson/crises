7553d3ec4c5ffb2891beae8bf869f1996ba7f135
JC BUISSON <buisson@enseeiht.fr>
2024-06-04 12:19:01 +0100

    WIP

--- backend/prisma/migrations/20240430152738_aaa/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "crise" (
-    "id" SERIAL NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "intensity" INTEGER NOT NULL,
-
-    CONSTRAINT "crise_pkey" PRIMARY KEY ("id")
-);
